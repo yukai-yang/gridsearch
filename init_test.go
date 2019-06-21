cdcf57d15ed3af80d674abf71389779355f69cd0
Yukai Yang <yukai.yang@gmail.com>
2019-06-21 15:05:41 +0200

    finish the tests

--- init_test.go
+++ init_test.go
@@ -7,12 +7,29 @@ import (
 
 func TestInit(t *testing.T) {
 	fmt.Println("=======================")
-	fmt.Println("Results from TestInit")
+	fmt.Println("Test results from TestInit")
+	fmt.Println("-----------------------")
+	fmt.Println("//Go code:")
+	fmt.Println("g := InitGrid([]float64{1, 2, 3}, []float64{1, 3, 2})")
+	fmt.Println("fmt.Println(g)")
+	fmt.Println("-----------------------")
+	fmt.Println("//results:")
 	g := InitGrid([]float64{1, 2, 3}, []float64{1, 3, 2})
 	fmt.Println(g)
 
+	fmt.Println("-----------------------")
+	fmt.Println("Go code:")
+	fmt.Println("g.Append(FromToBy(1, 0, -.2))")
+	fmt.Println("g.Append(FromToLen(1, 0, 5))")
+	fmt.Println("g.SetNumGoRoutines(2)")
+	fmt.Println("g.SetZoom(1)")
+	fmt.Println("g.SetDecay(.8)")
+	fmt.Println("g.SetNumReturn(3)")
+	fmt.Println("fmt.Println(g)")
+	fmt.Println("g.Append(FromToLen(1, 0, 5))")
+	fmt.Println("-----------------------")
+	fmt.Println("results:")
 	g.Append(FromToBy(1, 0, -.2))
-
 	g.Append(FromToLen(1, 0, 5))
 
 	g.SetNumGoRoutines(2)
--- search_test.go
+++ search_test.go
@@ -7,14 +7,27 @@ import (
 
 func TestSearch(t *testing.T) {
 	fmt.Println("=======================")
-	fmt.Println("Restuls from TestSearch")
-	g := InitGrid()
+	fmt.Println("Test restuls from TestSearch")
+	fmt.Println("-----------------------")
+	fmt.Println("//Go code:")
+	fmt.Println("g := InitGrid()")
+	fmt.Println("var tmp = FromToBy(-5.12, 5.12, .1)")
+	fmt.Println("g.Append(tmp, tmp)")
+	fmt.Println("g.SetNumGoRoutines(2)")
+	fmt.Println("g.SetZoom(2)")
+	fmt.Println("g.SetNumReturn(2)")
+	fmt.Println("var ret, val = g.Search(Rastrigin)")
+	fmt.Println("fmt.Println(ret, val)")
+	fmt.Println("fmt.Println(Rastrigin(ret[0]))")
+	fmt.Println("-----------------------")
+	fmt.Println("results:")
 
+	g := InitGrid()
 	var tmp = FromToBy(-5.12, 5.12, .1)
 	g.Append(tmp, tmp)
-
-	g.SetNumGoRoutines(1)
-	g.SetNumReturn(3)
+	g.SetNumGoRoutines(2)
+	g.SetZoom(2)
+	g.SetNumReturn(2)
 
 	var ret, val = g.Search(Rastrigin)
 	fmt.Println(ret, val)
